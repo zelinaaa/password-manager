d99c5dd9ab6b16f112ff06d02eb7ed55ed454c3d
Jakub Semerad <247606@vutbr.cz>
2024-05-03 02:50:45 +0200

    clean up

--- src/main.c
+++ src/main.c
@@ -1,27 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include "../header/cryptodef.h"
 
 int main() {
-	unsigned char * key;
-	unsigned char * iv;
-	unsigned char * hash;
-	unsigned char * salt;
-	initVault("test");
-	readMasterPassword("test", &key, &iv, &salt, &hash);
 
-	int resultAuthentication = verifyHash(hash, salt);
-
-	addPassword("test", key, iv);
-	addPassword("test", key, iv);
-	readPasswords("test");
-	printAllPasswords();
-	editPassword("test", key, iv, 1);
-	readPasswords("test");
-	printAllPasswords();
-	showDecryptedPassword("test", key, iv, 1);
-
-    return 0;
 }
 
