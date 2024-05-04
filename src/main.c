ef7c849306520397000ab95a10709373b6ad84ac
Jakub Semerad <247606@vutbr.cz>
2024-05-04 16:49:44 +0200

    -

--- src/main.c
+++ src/main.c
@@ -5,14 +5,14 @@
 
 int main(int argc, char *argv[]) {
 	if (argc < 3) {
-		printf("Usage: %s -i <filename> (init vault)\n", argv[0]);
-		printf("Usage: %s -i <filename> -d (delete vault after authentication)\n", argv[0]);
+		printf("Usage: %s -i <filename> (create new file to store passwords in)\n", argv[0]);
+		printf("       %s -i <filename> -d (delete file containing passwords)\n", argv[0]);
 		printf("       %s -f <filename> -a <service name> (add entry)\n", argv[0]);
 		printf("       %s -f <filename> -d <service name> (delete entry)\n", argv[0]);
-		printf("       %s -f <filename> -r (read file)\n", argv[0]);
-		printf("       %s -f <filename> -r <service name> (read service entry, decrypted)\n", argv[0]);
+		printf("       %s -f <filename> -r (list all entries in the file with passwords)\n", argv[0]);
+		printf("       %s -f <filename> -r <service name> (show decrypted password of an entry)\n", argv[0]);
 		printf("       %s -f <filename> -e <service name> (edit entry)\n", argv[0]);
-		printf("       %s -f <filename> -e (edit master password)\n", argv[0]);
+		printf("       %s -f <filename> -e (edit master password entry)\n", argv[0]);
 		return 1;
 	}
 
@@ -38,28 +38,24 @@ int main(int argc, char *argv[]) {
 				return 1;
 			}
 			addService(argv[2], argv[4]);
-			//printf("%s , %s - add entry", argv[2], argv[4]);
 		} else if (strcmp(operation, "-d") == 0) {
 			if (argc != 5) {
 				printf("Invalid arguments for delete entry\n");
 				return 1;
 			}
-			//deleteEntry(fileName, argv[4]);
-			printf("%s , %s - delete entry", argv[2], argv[4]);
+			deleteEntry(fileName, argv[4]); // implement
 		} else if (strcmp(operation, "-r") == 0) {
 			if (argc == 4) {
-				//readFile(fileName);
+				listAllServices(fileName); //fix
 				printf("%s - read file", argv[2]);
 			} else if (argc == 5) {
-				//readServiceEntry(fileName, argv[4]);
-				printf("%s , %s - read service, decrypted", argv[2], argv[4]);
+				readServicePassword(fileName, argv[4]); //implement
 			} else {
 				printf("Invalid arguments for read operation\n");
 				return 1;
 			}
 		} else if (strcmp(operation, "-e") == 0) {
 			if (argc == 4) {
-				//readFile(fileName);
 				editMaster(argv[2]);
 			} else if (argc == 5) {
 				editEntry(argv[2], argv[4]);
